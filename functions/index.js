29e5441da5c0ad2f303c66a53a27b9e4fcc9710c
evpavlov <evgenii.pavlov@t-systems.com>
2019-12-01 15:44:50 +0300

    Getting form data

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,30 @@
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 const cors = require("cors")({ origin: true });
-const {createReadStream: readStr} = require("fs");
+const { createReadStream: readStr } = require("fs");
 const mailTransport = nodemailer.createTransport({
-  service: "gmail"
+  service: "gmail",
+  secure: false,
+  port: 25,
+  auth: { user: "quemsaurose2@gmail.com", pass: "" },
+  tls: { rejectUnauthorized: false }
 });
-
 exports.mailer = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     if (req.method !== "POST") {
-          readStr("./index.html").pipe(req.res);
-      return; 
+      readStr("./index.html").pipe(req.res);
+      return;
     }
-
-    // const mailOptions = {
-    //   from: req.body.email,
-    //   replyTo: req.body.email,
-    //   to: "quemsaurose@gmail.com",
-    //   subject: `from my website ${req.body.email}`,
-    //   text: req.body.message,
-    //   html: `<p>${req.body.message}`
-    // };
-    console.log(JSON.stringify(req.body));
-
-//    mailTransport.sendMail(mailOptions);
-
+    const mailOptions = {
+      from: "quemsaurose2@gmail.com",
+      replyTo: "quemsaurose2@gmail.com",
+      to: "quemsaurose@gmail.com",
+      subject: `from my website ${req.body.email}`,
+      text: req.body.message,
+      html: `<p>${req.body.message}`
+    };
+    console.log("TCL: mailOptions", JSON.stringify(mailOptions));
+    mailTransport.sendMail(mailOptions);
     res.status(200).end();
-    // or you can pass data to indicate success.
-    // res.status(200).send({isEmailSend: true});
   });
 });
