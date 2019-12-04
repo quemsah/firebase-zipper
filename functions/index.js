2022edaba46baed8ce4b71d9897beb4da29aa9b3
evpavlov <evgenii.pavlov@t-systems.com>
2019-12-04 18:20:45 +0300

    zipper init
    Co-Authored-By: Евгений <quemsaurose@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,43 @@
 const functions = require("firebase-functions");
-const nodemailer = require("nodemailer");
-const cors = require("cors")({ origin: true });
 const { createReadStream: readStr } = require("fs");
-const mailTransport = nodemailer.createTransport({
-  service: "gmail",
-  secure: false,
-  port: 25,
-  auth: { user: "quemsaurose2@gmail.com", pass: "" },
-  tls: { rejectUnauthorized: false }
-});
-exports.mailer = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    if (req.method !== "POST") {
-      readStr("./index.html").pipe(req.res);
-      return;
-    }
-    const mailOptions = {
-      from: "quemsaurose2@gmail.com",
-      replyTo: "quemsaurose2@gmail.com",
-      to: "quemsaurose@gmail.com",
-      subject: `from my website ${req.body.email}`,
-      text: req.body.message,
-      html: `<p>${req.body.message}`
+const zlib = require("zlib");
+const Busboy = require("busboy");
+const cors = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers":
+    "cors,my,Content-Type,Accept,Access-Control-Allow-Headers"
+};
+
+exports.zipper = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    readStr("view/index.html").pipe(req.res);
+    return;
+  } else {
+    console.log("1");
+    const d = {
+      "Content-type": "application/zip",
+      "Content-disposition": "attachment; filename=result.zip"
     };
-    console.log("TCL: mailOptions", JSON.stringify(mailOptions));
-    mailTransport.sendMail(mailOptions);
-    res.status(200).end();
-  });
+    console.log("2");
+    res.writeHead(200, {
+      ...d,
+      ...cors
+    });
+    console.log("3");
+    const z = zlib.createGzip();
+    console.log("4");
+    const busboy = new Busboy({
+      headers: req.headers
+    });
+    console.log("5");
+    z.on("error", e => res.end("ERROR zip"));
+    console.log("6");
+    busboy
+      .on("file", (_, file) => file.pipe(z).pipe(res))
+      .on("error", e => res.end("ERROR boy"));
+    console.log("7");
+    req.pipe(busboy);
+    console.log("8");
+  }
 });
